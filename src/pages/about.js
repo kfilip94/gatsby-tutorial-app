e4db8a2d7dcfdd4569a57b979d724018abb845b8
Kamil Filip <kamil.filip94@gmail.com>
2020-02-28 17:33:42 +0000

    3. Linking Between Pages with Gatsby

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from 'gatsby';
 
 const AboutPage = () => {
   return (
     <div>
       <h1>About</h1>
       <p>My name is Kamil and I'm an awesome dev!</p>
+      <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
     </div>
   )
 };
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from 'gatsby';
 
 const ContactPage = () => {
   return (
     <div>
       <h1>Contact</h1>
       <p>You can reach me on kamil.filip94@gmail.com</p>
+      <p>Twitter: <a href="https://twitter.com/kfilip94" target="_blank">@kfilip94</a></p>
     </div>
   )
 };
