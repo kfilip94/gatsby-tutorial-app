c6ee2f12c92d31c62119c42c60dbeb83f9959e18
Kamil Filip <kamil.filip94@gmail.com>
2020-03-15 15:02:26 +0000

    18. Dynamic Pages from Contentful

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,38 +1,22 @@
 const path = require('path');
 
-module.exports.onCreateNode = ({ node, actions}) => {
-  const { createNodeField } = actions;
-
-  if(node.internal.type === 'MarkdownRemark') {
-    const slug = path.basename(node.fileAbsolutePath, '.md');
-
-    createNodeField({
-      node,
-      name: 'slug',
-      value: slug
-    });
-  }
-}
-
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
   const blogTemplate = path.resolve('./src/templates/blog.js');
   const response = await graphql(`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost {
         edges {
           node {
-            fields {
-              slug
-            }
+            slug
           }
         }
       }
     }
   `);
 
-  response.data.allMarkdownRemark.edges.forEach(edge => {
-    const { slug } = edge.node.fields;
+  response.data.allContentfulBlogPost.edges.forEach(edge => {
+    const { slug } = edge.node;
     createPage({
       component: blogTemplate,
       path: `/blog/${slug}`,
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import Layout from 'components/layout';
 import { graphql } from 'gatsby';
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 export const query = graphql`
   query ($slug: String!) {
-    markdownRemark ( fields: { slug: { eq: $slug }}) {
-      frontmatter {
-        title
-        date
+    contentfulBlogPost ( slug: { eq: $slug }) {
+      title
+      publishedDate(formatString:"MMMM Do, YYYY")
+      body {
+        json
       }
-      html
     }
   }
 `;
 
 const Blog = props => {
-  const { frontmatter, html } = props.data.markdownRemark;
-  const { title, date } = frontmatter;
+  const { title, publishedDate, body } = props.data.contentfulBlogPost;
+  const options = {
+    renderNode: {
+      "embedded-asset-block": (node) => {
+        const { title, file } = node.data.target.fields;
+        const alt = title['en-US'];
+        const url = file['en-US'].url;
+        return <img alt={alt} src={url} />
+      }
+    }
+  };
 
   return (
     <Layout>
       <h1>{title}</h1>
-      <p>{date}</p>
-      <div dangerouslySetInnerHTML={{__html: html}}></div>
+      <p>{publishedDate}</p>
+      {documentToReactComponents(body.json, options)}
     </Layout>
   )
 };
